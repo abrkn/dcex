2e971f205df5e09ebbc2d4927b9e563694d6625a
Andreas Brekken <andreas@brekken.com>
2018-10-18 13:47:44 +0700

    Remove unused code

--- docker-run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-docker run -ti \
-       --net=host \
-       hasura/graphql-engine:v1.0.0-alpha24 \
-       graphql-engine \
-       --database-url postgres://andreas@192.168.99.100/dcex_dev \
-       serve --enable-console
