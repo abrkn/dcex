91813fe49fef66e377925e4798d10e9216a3f997
Andreas Brekken <andreas@brekken.com>
2018-10-18 13:43:36 +0700

    Add link to Github

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,9 @@ const Header = ({ router: { pathname } }) => (
     <Link prefetch href="/about">
       <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
     </Link>
+    <a href="https://github.com/abrkn/dcex" target="_blank">
+      Source code (Github)
+    </a>
     <style jsx>{`
       header {
         margin-bottom: 25px;
