c3feb71c974b6ad145bb0f6f962c5a02d5c2ef55
Andreas Brekken <andreas@brekken.com>
2018-10-25 13:20:57 +0700

    Remove unused code

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import App from '../components/App';
 import Header from '../components/Header';
-// import Submit from '../components/Submit';
 import BlockList from '../components/BlockList';
 
 export default () => (
   <App>
     <Header />
-    {/* <Submit /> */}
     <BlockList />
   </App>
 );
