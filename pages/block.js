9c2124de38903c164e05f25205c7c56e0cba5313
Andreas Brekken <andreas@brekken.com>
2018-10-25 13:26:51 +0700

    Cleanup block page

--- pages/block.js
+++ pages/block.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import App from '../components/App';
 import Header from '../components/Header';
-// import Submit from '../components/Submit';
 import Block from '../components/Block';
 
-// export default (...args) => {
-//   console.log('PAGE', args);
-
-//   return (
-//     <App>
-//       <Header />
-//       <Block />
-//     </App>
-//   );
-// };
-
-export default class Blog extends React.Component {
+export default class BlockPage extends React.Component {
   static async getInitialProps({ query }) {
     return { query };
   }
