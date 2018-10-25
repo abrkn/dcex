9fbc2a474d2fc162455e892bb160cb67e3e6ecef
Andreas Brekken <andreas@brekken.com>
2018-10-25 19:06:20 +0700

    Add page title to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import App from '../components/App';
 import Header from '../components/Header';
 import BlockList from '../components/BlockList';
+import Head from 'next/head';
+import { chain } from '../frontendUtils';
+
+const { titlePrefix } = chain;
 
 export default () => (
   <App>
+    <Head>
+      <title>
+        {titlePrefix}
+        Explorer
+      </title>
+    </Head>
     <Header />
     <BlockList />
   </App>
